e800536c3a2e5e9ebe95e6794531895144e4eb7e
agreeshvs <agreesh777@gmail.com>
2025-08-22 14:54:06 +0530

    Injecting dependency

--- src/app/header/home/hero/hero.component.ts
+++ src/app/header/home/hero/hero.component.ts
@@ -4,15 +4,19 @@ import { SubscribeService } from 'src/app/Services/subscribe.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
+  providers: [SubscribeService]
 })
 export class HeroComponent {
+  constructor(private subSubscribe: SubscribeService) { }
   OnSubscribe(){
     // Logic for subscription can be added here
     // database call, API call, etc.
     // send email to user, etc.
     // alert('Subscribed successfully!');
-    let subscriveService = new SubscribeService();
-    subscriveService.onSubscribeClicked();
+    /* let subscriveService = new SubscribeService();
+    subscriveService.onSubscribeClicked('hero component'); */
+
+    this.subSubscribe.onSubscribeClicked('hero component');
   }
 }
