3b58da574a51e5a848648861a9671443ff037560
agreeshvs <agreesh777@gmail.com>
2025-08-22 17:56:32 +0530

    Injecting service into another service

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,11 +1,17 @@
+import { Injectable } from "@angular/core";
 import { User } from "../Models/User";
+import { LoggerService } from "./logger.service";
 
+@Injectable() 
+// Injectable decorator allows this service to be injected into components or other services
 export class UserService{
     users: User[] = [
         new User('Steve Smith','Male','Monthly','Active'),
         new User('Mary Johnson','Female','Yearly','Inactive'),
         new User('John Smith', 'Male', 'Quarterly', 'Active'),
     ];
+
+    constructor(private loggerService: LoggerService) {}
 
 
     getAllUsers(): User[]{
@@ -15,5 +21,10 @@ export class UserService{
     createUser(name: string,gender: string,subType: string,status: string){
         let user = new User(name,gender,subType,status);
         this.users.push(user);
+
+        /* let logger = new LoggerService();
+        logger.logMessage(name,status) */
+
+        this.loggerService.logMessage(name,status);
     }
 }
